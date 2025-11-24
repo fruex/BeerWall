1e3682e3b4ed5e4fc9e6595695953b994c8f79a6
Adrian Botor <adrian.botor@lidl.pl>
2025-11-24 13:46:51 +0100

    Rename package and update activity reference in AndroidManifest.xml

--- android-app/src/MainActivity.kt
+++ android-app/src/MainActivity.kt
@@ -2,7 +2,7 @@
  * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package hello.world
+package com.igibeer.beerwall
 
 import Screen
 import android.os.Bundle
