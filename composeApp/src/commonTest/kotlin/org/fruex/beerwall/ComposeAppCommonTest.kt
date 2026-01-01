db534709df6a8050e788025659bb11cd49bb0067
Adrian Botor <adrian.botor@lidl.pl>
2026-01-01 16:54:06 +0100

    Apply kmp from wizard

---
+++ composeApp/src/commonTest/kotlin/org/fruex/beerwall/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package org.fruex.beerwall
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
