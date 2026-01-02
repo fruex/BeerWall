d591940c1070f4cc3c3d741c981296992db7c8af
Adrian Botor <adrian.botor@gmail.com>
2026-01-02 10:23:01 +0100

    Update dependency references in `build.gradle.kts` to use `libs` and fix typo in `libs.versions.toml` (`gogleid` → `googleid`)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -28,9 +28,9 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
-            implementation("androidx.credentials:credentials:1.3.0")
-            implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
-            implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+            implementation(libs.androidx.credentials)
+            implementation(libs.androidx.credentials.play)
+            implementation(libs.googleid)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
