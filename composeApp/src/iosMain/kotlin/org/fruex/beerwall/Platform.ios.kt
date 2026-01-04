f55e887323fb821b1c1a6f4e7315c1f2a8723df4
Adrian Botor <adrian.botor@gmail.com>
2026-01-04 11:14:54 +0100

    Update API base URLs for iOS and test configurations; adjust save card logic in AddCardScreen

--- composeApp/src/iosMain/kotlin/org/fruex/beerwall/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/org/fruex/beerwall/Platform.ios.kt
@@ -4,7 +4,8 @@ import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-    override val apiBaseUrl: String = "http://localhost:5254/api"
+//    override val apiBaseUrl: String = "http://localhost:5254/api"
+    override val apiBaseUrl: String = "https://beerwall-apitest-cjcfgfehh9grhne5.polandcentral-01.azurewebsites.net/api"
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
