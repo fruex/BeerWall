507e013f3570c0f5701c6fa3fbd416626c3884ef
Adrian Botor <adrian.botor@gmail.com>
2026-01-04 11:03:49 +0100

    Set up CI/CD workflow for Android build and release, update API base URLs across projects, and adjust test configurations.

--- composeApp/src/androidMain/kotlin/org/fruex/beerwall/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/org/fruex/beerwall/Platform.android.kt
@@ -4,7 +4,8 @@ import android.os.Build
 
 class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
-    override val apiBaseUrl: String = "http://localhost:5254/api"
+//    override val apiBaseUrl: String = "http://localhost:5254/api"
+    override val apiBaseUrl: String = "https://beerwall-apitest-cjcfgfehh9grhne5.polandcentral-01.azurewebsites.net/api"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
