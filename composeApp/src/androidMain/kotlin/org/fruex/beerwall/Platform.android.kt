6ea53c69307c5543cdbd3a816e9a3711282a26f0
Adrian Botor <adrian.botor@gmail.com>
2026-01-09 19:03:39 +0100

    Ujednolicenie api config url

--- composeApp/src/androidMain/kotlin/org/fruex/beerwall/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/org/fruex/beerwall/Platform.android.kt
@@ -4,8 +4,6 @@ import android.os.Build
 
 class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
-//    override val apiBaseUrl: String = "http://localhost:5254/api"
-    override val apiBaseUrl: String = "https://beerwall-apitest-cjcfgfehh9grhne5.polandcentral-01.azurewebsites.net/api"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
---
+++ composeApp/src/commonMain/kotlin/org/fruex/beerwall/data/remote/ApiConfig.kt
@@ -0,0 +1,7 @@
+package org.fruex.beerwall.data.remote
+
+object ApiConfig {
+//     const val BASE_URL = "http://localhost:5254/api"
+//    const val BASE_URL = "https://beerwall-apitest-cjcfgfehh9grhne5.polandcentral-01.azurewebsites.net/api"
+    const val BASE_URL = "http://api-debug.igibeer.pl:7000"
+}
