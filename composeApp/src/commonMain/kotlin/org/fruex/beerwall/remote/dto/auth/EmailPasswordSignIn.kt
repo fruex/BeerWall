dbca5ea0ce2856cb66e10d7a90249a7eb36333e9
Adrian Botor <adrian.botor@gmail.com>
2026-01-14 19:34:08 +0100

    Update email sign-in to use `EmailPasswordSignInEnvelope`; remove unused methods from `BeerWallViewModel`

--- composeApp/src/commonMain/kotlin/org/fruex/beerwall/remote/dto/auth/EmailPasswordSignIn.kt
+++ composeApp/src/commonMain/kotlin/org/fruex/beerwall/remote/dto/auth/EmailPasswordSignIn.kt
@@ -1,6 +1,7 @@
 package org.fruex.beerwall.remote.dto.auth
 
 import kotlinx.serialization.Serializable
+import org.fruex.beerwall.remote.common.ApiEnvelope
 
 @Serializable
 data class EmailPasswordSignInRequest(
@@ -9,3 +10,4 @@ data class EmailPasswordSignInRequest(
 )
 
 typealias EmailPasswordSignInResponse = AuthResponse
+typealias EmailPasswordSignInEnvelope = ApiEnvelope<EmailPasswordSignInResponse>
