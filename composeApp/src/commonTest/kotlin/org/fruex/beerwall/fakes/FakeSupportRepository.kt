c529a63222ac1825d41a13e53808d50a8d1cdf19
Adrian Botor <adrian.botor@gmail.com>
2026-01-16 22:15:24 +0100

    Dodanie testów jednostkowych dla UseCases i ViewModelu\n\n- Dodano klasę bazową `BaseTest` do obsługi korutyn w testach.\n- Zaimplementowano ręczne mocki (Fakes) dla repozytoriów: Auth, Balance, Card, Support, Transaction.\n- Dodano testy dla kluczowych UseCases: `GetBalances`, `ToggleCardStatus`, `EmailPasswordSignIn`.\n- Dodano testy dla `BeerWallViewModel` weryfikujące logowanie i stany UI.

---
+++ composeApp/src/commonTest/kotlin/org/fruex/beerwall/fakes/FakeSupportRepository.kt
@@ -0,0 +1,13 @@
+package org.fruex.beerwall.fakes
+
+import org.fruex.beerwall.domain.repository.SupportRepository
+
+class FakeSupportRepository : SupportRepository {
+    var shouldFail = false
+    var failureMessage = "Błąd wysyłania wiadomości"
+
+    override suspend fun sendMessage(message: String): Result<Unit> {
+        if (shouldFail) return Result.failure(Exception(failureMessage))
+        return Result.success(Unit)
+    }
+}
---
+++ composeApp/src/commonTest/kotlin/org/fruex/beerwall/test/BaseTest.kt
@@ -0,0 +1,25 @@
+package org.fruex.beerwall.test
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+
+@OptIn(ExperimentalCoroutinesApi::class)
+abstract class BaseTest {
+    protected val testDispatcher: TestDispatcher = StandardTestDispatcher()
+
+    @BeforeTest
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @AfterTest
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+}
