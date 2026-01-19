815f8f65c6e91c219c1d8f8267f090917aabe900
Adrian Botor <adrian.botor@gmail.com>
2026-01-19 12:06:35 +0100

    Add `KoinModulesVerificationTest` for validating DI modules - Remove deprecated `checkModules()` usage from debug builds. - Add Koin test dependency for comprehensive validation.

--- composeApp/src/iosMain/kotlin/com/fruex/beerwall/di/KoinHelper.kt
+++ composeApp/src/iosMain/kotlin/com/fruex/beerwall/di/KoinHelper.kt
@@ -1,6 +1,5 @@
 package com.fruex.beerwall.di
 
-import com.fruex.beerwall.BuildKonfig
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import org.koin.core.logger.PrintLogger
@@ -9,8 +8,5 @@ fun initKoin() {
     startKoin {
         logger(PrintLogger(Level.INFO))
         modules(appModules())
-        if (BuildKonfig.DEBUG) {
-            checkModules()
-        }
     }
 }
